0f8f938f21e82018a37883f6bca893d25c2a45bd
kkamara <kamaracomputers@gmail.com>
2024-01-16 13:43:24 +0000

    Add Stateless Widget & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text("www.kelvinkamara.com"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: const Center(
-      child: Text(
-        "Hello World",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey,
-          fontFamily: "IndieFlower",
+void main() => runApp(const MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("www.kelvinkamara.com"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: "IndieFlower",
+          ),
         ),
       ),
-    ),
-    floatingActionButton: const FloatingActionButton(
-      onPressed: null,
-      backgroundColor: Colors.red,
-      child: Text("Click"),
-    ),
-  ),
-));
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.red,
+        child: Text("Click"),
+      ),
+    );
+  }
+}
