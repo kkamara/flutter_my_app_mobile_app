e791478fd4fccc6fff85cc7044aff736965e4b11
kkamara <kamaracomputers@gmail.com>
2024-01-16 13:04:14 +0000

    Add Scaffold and AppBar Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Text("Hello World"),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text("www.kelvinkamara.com"),
+      centerTitle: true,
+    ),
+    body: const Center(
+      child: Text("Hello World"),
+    ),
+    floatingActionButton: const FloatingActionButton(
+      onPressed: null,
+      child: Text("Click"),
+    ),
+  ),
 ));
